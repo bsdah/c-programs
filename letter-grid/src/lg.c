42b23a15cbee8f33e742ad2758fb9d76e4f1306a
Ali Hameed <bsdah@protonmail.com>
2024-12-06 05:10:41 +0200

    Rename file

---
+++ letter-grid/src/lg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("A\tB\tC\n");
+    printf("D\tE\tF\n");
+    printf("G\tH\tI\n");
+    printf("J\tK\tL\n");
+    printf("M\tN\tO\n");
+    printf("P\tQ\tR\n");
+    printf("S\tT\tU\n");
+    printf("V\tW\tX\n");
+    printf("Y\tZ\n");
+    return 0;
+}
